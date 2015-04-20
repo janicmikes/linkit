3c29235befbb82c8a38e45985b51ef8bd5fdffc1
Janic Mikes <janic.mikes@gmail.com>
2015-04-20 19:20:21 +0200

    Started with part 02

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var linkRepo = require('../data/links');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/linkit', function (req, res, next) {
+    res.render('linkit', {"title": 'LinkIt - The Link Aggregator', "data": linkRepo});
 });
 
 module.exports = router;
