01dab0a20c9d4e5618cf919e4144e590e9d4857d
Janic Mikes <janic.mikes@gmail.com>
2015-04-20 19:01:03 +0200

    Part 01

---
+++ routes/links.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var linkRepo = require('../data/links');
+
+router.get('/', function(req, res, next) {
+  res.send(linkRepo);
+  //res.render('index', { title: 'Express' });
+});
+
+router.put('/', function(req, res, next) {
+  console.log('put');
+});
+
+router.delete('/:id', function(req, res, next) {
+  console.log('delete ' + req.param('id'));
+});
+
+router.post('/:id/up', function(req, res, next) {
+  console.log('upvote ' + req.param('id'));
+});
+
+router.post('/:id/down', function(req, res, next) {
+  console.log('downvote ' + req.param('id'));
+});
+
+module.exports = router;
